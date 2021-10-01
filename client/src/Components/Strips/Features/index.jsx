a9ebb120c156de17723fe60544ac0333054ff8ee
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2021-10-01 11:18:34 +0530

    add features component

--- client/src/Components/Strips/Features/index.jsx
+++ client/src/Components/Strips/Features/index.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 
+import './style.scss';
+
 const Features = () => {
     return ( 
-        <h1>Features</h1>
+        <main className="features-section">
+            <div className="features">
+                <div class="feature">
+                    <img 
+                        alt=""
+                        className="center pt-4 pb-4 mt-5 mb-3"
+                        src="https://smarty-cards.s3.ap-south-1.amazonaws.com/resources/img/easy-to-use.webp" 
+                    />
+
+                    <div className="feature-text text-center mb-5">
+                        <p className="mb-0">Easy</p>
+                        <p>to Use</p>
+                    </div>
+                </div>
+
+                <div class="feature">
+                    <img 
+                        alt=""
+                        className="center pt-4 pb-4 mt-5 mb-3"
+                        src="https://smarty-cards.s3.ap-south-1.amazonaws.com/resources/img/intutive.webp" 
+                    />
+
+                    <div className="feature-text text-center mb-5">
+                        <p className="mb-0">Interactive and</p>
+                        <p>Intuitive</p>
+                    </div>
+                </div>
+
+                <div class="feature">
+                    <img 
+                        alt=""
+                        className="center pt-4 pb-4 mt-5 mb-3"
+                        src="https://smarty-cards.s3.ap-south-1.amazonaws.com/resources/img/progress-tracking.webp" 
+                    />
+
+                    <div className="feature-text text-center mb-5">
+                        <p className="mb-0">Progress</p>
+                        <p>Tracking</p>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
  
