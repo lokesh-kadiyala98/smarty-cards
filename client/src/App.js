02cfda9aa93651ffbfbc8f559634c1c1bb6a6784
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2021-10-26 17:58:52 +0530

    init app with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import configureStore from './store/configureStore';
 import Loader from './Components/Loader';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
-const Home = React.lazy(() => import('./Components/Home'));
-const TemplatePicker = React.lazy(() => import('./Components/TemplatePicker'));
+
+const Home = React.lazy(() => import('./views/Home'));
+const TryOut = React.lazy(() => import('./views/TryOut'));
 const NotFound = React.lazy(() => import('./Components/Strips/NotFound'));
+
+const store = configureStore()
 
 class App extends React.Component {
   render() { 
     return (
-      <React.Suspense fallback={<Loader />}>
-        <Switch>
-          <Route path="/choose-template" component={TemplatePicker} />
-          <Route path="/not-found" component={NotFound} />
-          <Route exact path="/" component={Home} />
-          <Redirect to="/not-found" />
-        </Switch>
-      </React.Suspense>
+      <Provider store={store}>
+        <React.Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path="/try-out" component={TryOut} />
+            <Route path="/not-found" component={NotFound} />
+            <Route exact path="/" component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </React.Suspense>
+      </Provider>
     );
   }
 }
